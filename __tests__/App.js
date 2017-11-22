0b60df108d2c6d0cd6a0f20c9e64767d7f81e05c
Andrzej Kopeć <kapkowski@gmail.com>
2017-11-22 19:46:40 +0100

    Add codepush

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,10 +1,7 @@
-import 'react-native'
-import React from 'react'
-import App from '../App'
+/* eslint-disable fp/no-unused-expression, fp/no-nil */
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
-
-it('renders correctly', () => {
-    const tree = renderer.create(<App />)
+describe('foo', () => {
+    it('is true', () => {
+        expect('foo').toBe('foo')
+    })
 })
